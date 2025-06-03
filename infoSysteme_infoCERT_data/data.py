f386515aade774b1ed8e7a4f64a4d7b69237d7dc
Haytham <berhilih50@gmail.com>
2025-06-03 23:12:15 +0200

    Update data.py

--- infoSysteme_infoCERT_data/data.py
+++ infoSysteme_infoCERT_data/data.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime, timedelta, UTC
 
 
-DB_FILE = "/home/admin/AMS/partie2/database.db"
+DB_FILE = "/home/admin/AMS/infoSysteme_infoCERT_data/database.db"
 
 def creation_table():
     
